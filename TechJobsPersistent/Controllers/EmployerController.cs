a9c4d867cf078b637fdb5ef7eb985c38a9401344
Sabeena <sabeenarayyan13@gmail.com>
2021-10-25 16:34:30 -0500

    added addjob and addemployer viewmodels

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -13,23 +15,66 @@ namespace TechJobsPersistent.Controllers
     public class EmployerController : Controller
     {
         // GET: /<controller>/
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+
+            return View(employers);
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                string name = addEmployerViewModel.Name;
+                string location = addEmployerViewModel.Location;
+
+                List<Employer> existingItems = context.Employers
+                    .Where(el => el.Name == name)
+                    .Where(el => el.Location == location)
+                    .ToList();
+
+                if(existingItems.Count == 0)
+                {
+                    Employer newEmployer = new Employer
+                    {
+                        Name = addEmployerViewModel.Name,
+                        Location = addEmployerViewModel.Location
+                    };
+                    context.Employers.Add(newEmployer);
+                    context.SaveChanges();
+                    }
+                return Redirect("/Employers");
+
+            }
+            else
+            {
+                ViewBag.error = "The location you entered is already exists.";
+                return View(addEmployerViewModel);
+            }
         }
+            
 
         public IActionResult About(int id)
         {
+           // Employer theEmployer = context.employers
+               // .Include(e => e.Location)
+                //.Single(e => e.Id == id);
+            //AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel(theEmployer);
+
+
             return View();
         }
     }
--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,7 +1,22 @@
 --Part 1
---Id - type int
---Name - type string
---EmployerId - type int(refer Employer object)
+--##EMPLOYER
+--Id - type int A1 PK
+--Name - type LONGTEXT
+--Location - type LONGTEXT
+
+--##JOBS
+-- ID - type int AI PK
+-- NAME - LONGTEXT
+--EmployerID - INT
+
+--##JOBSKILLS
+--JobId - int PK
+--SkillId -INT PK
+
+--##SKILLS
+--Id - int AI PK
+--Name - longtext
+--Description - longtext
 
 --Part 2
 
